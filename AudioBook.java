10d0d6574fd46b30c8c0a7bbd722f3052a6c5ae3
unknown <prabhnoorsingh85@gmail.com>
2025-11-20 10:32:31 +0100

    added comments for better understanding

--- AudioBook.java
+++ AudioBook.java
@@ -1,7 +1,11 @@
 public class AudioBook extends Book {
+    //making the runtime as private for security
     private int runTime;
+    //modifying the constructor for the AudioBook class
     AudioBook(String title, String author, int runTime){
+        // using the super command to reference the book class
         super(title, author, 0);
+        //using this. command to only affect the elements in this class
         this.runTime = runTime;
     }
 }
--- EBook.java
+++ EBook.java
@@ -1,7 +1,11 @@
 public class EBook extends Book {
+    //making the format as private for security
     private String format;
+    //modifying the constructor for the EBook class
     EBook(String title, String author,int pageCount, String format){
+        // using the super command to reference the book class
         super(title, author, pageCount);
+        //using this. command to only affect the elements in this class
         this.format = format;
     }
 }
